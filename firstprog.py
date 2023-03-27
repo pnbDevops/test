298d54a0c4e54b3bd8ab270edccec60d0566a5b1
Bharavi <narasimhabharavi9@gmail.com>
2023-03-27 12:44:26 +0530

    added code to know from users

--- firstprog.py
+++ firstprog.py
@@ -1,1 +1,9 @@
 print('hello everyone this is narasimha bharavi potturi')
+
+hungry=input("What do you want to have....\n")
+
+if(hungry=="yes"):
+    print("samsosas and tea .....")
+
+else:
+    print("Ok no issues but eat something")
